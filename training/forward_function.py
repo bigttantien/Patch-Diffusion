3b34de3ccc030817a7532ac4f4f2229bdca62187
bigttantien <bigttantien@gmail.com>
2024-04-11 23:05:17 +0700

    new loss edm

--- training/forward_function.py
+++ training/forward_function.py
@@ -23,23 +23,20 @@ def set_requires_grad(model, value):
 
 @persistence.persistent_class
 class forward_class:
-    def __init__(self, beta_d=19.9, beta_min=0.1, epsilon_t=1e-5):
-        self.beta_d = beta_d
-        self.beta_min = beta_min
-        self.epsilon_t = epsilon_t
-    
-    def __call__(self, net, images, labels, augment_pipe=None):
-        rnd_uniform = torch.rand([images.shape[0], 1, 1, 1], device=images.device)
-        sigma = self.sigma(1 + rnd_uniform * (self.epsilon_t - 1))
-        weight = 1 / sigma ** 2
+    def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5):
+        self.P_mean = P_mean
+        self.P_std = P_std
+        self.sigma_data = sigma_data
+
+    def __call__(self, net, images, labels=None, augment_pipe=None):
+        rnd_normal = torch.randn([images.shape[0], 1, 1, 1], device=images.device)
+        sigma = (rnd_normal * self.P_std + self.P_mean).exp()
+        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
         y, augment_labels = augment_pipe(images) if augment_pipe is not None else (images, None)
         n = torch.randn_like(y) * sigma
-        D_yn = net(x=y + n, sigma=sigma, class_labels=labels, augment_labels=augment_labels)
+        D_yn = net(y + n, sigma, labels, augment_labels=augment_labels)
+        # loss = weight * ((D_yn - y) ** 2)
         return D_yn
-
-    def sigma(self, t):
-        t = torch.as_tensor(t)
-        return ((0.5 * self.beta_d * (t ** 2) + self.beta_min * t).exp() - 1).sqrt()
 
 #----------------------------------------------------------------------------
 
